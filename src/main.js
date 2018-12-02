5fc4fc6c8d015603f70c2a1e5ef37530adb32f7d
sunxiantong <&7177210285xX>
2018-12-02 18:03:11 +0800

    layout

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/style/total.less';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+Vue.use(elementUI);
+
 new Vue({
   el: '#app',
   router,
---
+++ src/view/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <el-container>
+        <el-header class="header">
+            <div class="header-wrap">
+                <div class="header-logo">
+
+                </div>
+                <div class="header-drop">
+
+                </div>
+            </div>
+        </el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+	export default {
+		name: "index",
+        data(){
+		    return {
+
+            }
+        },
+        methods: {
+
+        }
+	}
+</script>
+
+<style scoped>
+    .header-wrap{
+        margin: 0 auto;
+        width: 65%;
+        height: 60px;
+        justify-content: space-between;
+    }
+    .header-logo{
+        display: inline-block;
+        height: 100%;
+        width: 320px;
+        background-color: #404241;
+    }
+    .header-drop{
+        display: inline-block;
+        height: 100%;
+        width: 600px;
+        background-color: black;
+    }
+</style>
