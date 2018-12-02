45f9141e42f85fd7e823d8652608107175089b6d
sunxiantong <sunmarktoto@163.com>
2018-12-02 20:54:28 +0800

    jpg

--- src/view/index.vue
+++ src/view/index.vue
@@ -3,7 +3,7 @@
         <el-header class="header">
             <div class="header-wrap">
                 <div class="header-logo">
-
+                    <img src="../assets/image/logo.jpg" alt="">
                 </div>
                 <div class="header-drop">
 
@@ -30,22 +30,5 @@
 </script>
 
 <style scoped>
-    .header-wrap{
-        margin: 0 auto;
-        width: 65%;
-        height: 60px;
-        justify-content: space-between;
-    }
-    .header-logo{
-        display: inline-block;
-        height: 100%;
-        width: 320px;
-        background-color: #404241;
-    }
-    .header-drop{
-        display: inline-block;
-        height: 100%;
-        width: 600px;
-        background-color: black;
-    }
+
 </style>
